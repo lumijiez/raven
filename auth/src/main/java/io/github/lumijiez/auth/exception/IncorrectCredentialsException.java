fd98743979e8008457d98d0e6775bcaf5f113a5b
Friptu Ludmila <144111978+FriptuLudmila@users.noreply.github.com>
2025-01-26 00:27:06 +0200

    Clean useless exception headers

--- auth/src/main/java/io/github/lumijiez/auth/exception/IncorrectCredentialsException.java
+++ auth/src/main/java/io/github/lumijiez/auth/exception/IncorrectCredentialsException.java
@@ -3,7 +3,6 @@ package io.github.lumijiez.auth.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class IncorrectCredentialsException extends AuthException {
     public IncorrectCredentialsException() {
         super("Incorrect credentials, either email or password");
