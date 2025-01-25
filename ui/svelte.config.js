d03d06871839827640c90131ed996a9ddfc09236
Daniel <59575049+lumijiez@users.noreply.github.com>
2025-01-25 23:38:54 +0200

    fix too generic headers

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -3,17 +3,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		csp: {
-			directives: {
-				'script-src': ['nonce-', 'strict-dynamic']
-			},
-			reportOnly: {
-				'script-src': ['nonce-', 'strict-dynamic'],
-				'report-uri': ['/']
-			},
-			mode: "auto"
-		}
+		adapter: adapter()
 	}
 };
 
