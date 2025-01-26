6d6abdcc2ee4ddc4d432a61686af383905f7ff1f
Masha003 <54769374+Masha003@users.noreply.github.com>
2025-01-27 00:23:21 +0200

    Add all needed CSP headers in svelte config

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -3,7 +3,17 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			directives: {
+				'script-src': ['self', 'https://lumijiez.pw'],
+				'style-src': ['self', 'https://lumijiez.pw'],
+				'default-src': ['self', 'https://lumijiez.pw'],
+				'frame-ancestors': ['self', 'https://lumijiez.pw'],
+				'form-action': ['self', 'https://lumijiez.pw']
+			},
+			mode: "auto"
+		}
 	}
 };
 
