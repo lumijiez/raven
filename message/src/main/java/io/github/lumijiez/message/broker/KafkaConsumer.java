7042cda1c2efdd014838573957aa4d4a6105e69c
MarinBizarreAdventure <102982146+MarinBizarreAdventure@users.noreply.github.com>
2025-01-28 00:32:53 +0200

    Add messageservice to kafka

--- message/src/main/java/io/github/lumijiez/message/broker/KafkaConsumer.java
+++ message/src/main/java/io/github/lumijiez/message/broker/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package io.github.lumijiez.message.broker;
 
+import io.github.lumijiez.message.domain.msg.service.MessageService;
 import io.github.lumijiez.model.kafka.KafkaMessage;
 import io.github.lumijiez.model.kafka.KafkaMessageResponse;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,15 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
+    private final MessageService messageService;
     private final KafkaProducer producer;
 
-    public KafkaConsumer(KafkaProducer producer) {
+    public KafkaConsumer(KafkaProducer producer, MessageService messageService) {
         this.producer = producer;
+        this.messageService = messageService;
     }
 
     @KafkaListener(topics = "chat.requests", groupId = "chat-consumers")
     public void listenRequests(KafkaMessage kafkaMessage) {
         System.out.println("Received request: " + kafkaMessage);
+
         producer.sendToClient(new KafkaMessageResponse(kafkaMessage.getSenderId(), kafkaMessage.getChatId(), kafkaMessage.getTimestamp(), kafkaMessage.getContent()));
     }
 }
