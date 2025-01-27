35200e69ac4cc8c83da5bd0427289cae5c74b149
katymun <114353294+katymun@users.noreply.github.com>
2025-01-27 19:55:33 +0200

    Proper DB for 2FA

--- auth/src/main/resources/db/migration/V1__create_user_table.sql
+++ auth/src/main/resources/db/migration/V1__create_user_table.sql
@@ -4,3 +4,19 @@ CREATE TABLE users (
                        email VARCHAR(100) NOT NULL,
                        password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE temp_users (
+                            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                            username VARCHAR(100) NOT NULL,
+                            email VARCHAR(100) NOT NULL,
+                            password VARCHAR(255) NOT NULL,
+                            verification_code VARCHAR(6) NOT NULL,
+                            created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE temp_logins (
+                             id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                             email VARCHAR(100) NOT NULL,
+                             verification_code VARCHAR(6) NOT NULL,
+                             created_at TIMESTAMP DEFAULT NOW()
+);
