79e2d0af33a092eb18aba5cf719a1598dfc338c7
katymun <114353294+katymun@users.noreply.github.com>
2025-01-27 20:28:07 +0200

    Clean DTOs

--- auth/src/main/java/io/github/lumijiez/auth/dto/response/UserDetailsDTO.java
+++ auth/src/main/java/io/github/lumijiez/auth/dto/response/UserDetailsDTO.java
@@ -1,6 +1,7 @@
 package io.github.lumijiez.auth.dto.response;
 
 
+import io.github.lumijiez.auth.domain.entity.User;
 import lombok.Data;
 
 import java.util.UUID;
@@ -10,4 +11,12 @@ public class UserDetailsDTO {
     private UUID id;
     private String username;
     private String email;
+
+    public static UserDetailsDTO from(User user) {
+        UserDetailsDTO dto = new UserDetailsDTO();
+        dto.setId(user.getId());
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        return dto;
+    }
 }
--- auth/src/main/java/io/github/lumijiez/auth/service/UserService.java
+++ auth/src/main/java/io/github/lumijiez/auth/service/UserService.java
@@ -3,14 +3,8 @@ package io.github.lumijiez.auth.service;
 import io.github.lumijiez.auth.domain.entity.User;
 import io.github.lumijiez.auth.dto.request.FindUserRequestDTO;
 import io.github.lumijiez.auth.dto.response.UserDetailsDTO;
-import io.github.lumijiez.auth.exception.IncorrectCredentialsException;
-import io.github.lumijiez.auth.exception.UserAlreadyExistsException;
 import io.github.lumijiez.auth.exception.UserNotFoundException;
 import io.github.lumijiez.auth.repository.UserRepository;
-import io.github.lumijiez.auth.dto.response.AuthResponseDTO;
-import io.github.lumijiez.auth.security.JwtHelper;
-import org.modelmapper.ModelMapper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,57 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
-    private final JwtHelper jwtHelper;
-    private final ModelMapper modelMapper;
 
-    public UserService(UserRepository userRepository,
-                       BCryptPasswordEncoder passwordEncoder,
-                       JwtHelper jwtHelper,
-                       ModelMapper modelMapper) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtHelper = jwtHelper;
-        this.modelMapper = modelMapper;
     }
-
-    @Transactional
-    public AuthResponseDTO registerUser(RegisterRequestDTO request) {
-        if (userRepository.existsByUsername(request.getUsername()))
-            throw new UserAlreadyExistsException("username", request.getUsername());
-
-        if (userRepository.existsByEmail(request.getEmail()))
-            throw new UserAlreadyExistsException("email", request.getEmail());
-
-        User user = modelMapper.map(request, User.class);
-        userRepository.save(user);
-
-        return authenticateUser(
-                LoginRequestDTO.builder()
-                        .usernameOrEmail(user.getUsername())
-                        .password(request.getPassword())
-                        .build());
-    }
-
-    @Transactional(readOnly = true)
-    public AuthResponseDTO authenticateUser(LoginRequestDTO request) {
-        User user = userRepository
-                .findByUsernameOrEmail(request.getUsernameOrEmail(), request.getUsernameOrEmail())
-                .orElseThrow(() -> new UserNotFoundException(request.getUsernameOrEmail()));
-
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
-            throw new IncorrectCredentialsException();
-
-        return AuthResponseDTO.from(jwtHelper.generateTokenForUser(user));
-    }
-
 
     @Transactional(readOnly = true)
     public UserDetailsDTO findUser(FindUserRequestDTO request) {
         User user = userRepository.findByUsernameOrEmail(request.getUsernameOrEmail(), request.getUsernameOrEmail())
                 .orElseThrow(() -> new UserNotFoundException(request.getUsernameOrEmail()));
 
-        return modelMapper.map(user, UserDetailsDTO.class);
+        return UserDetailsDTO.from(user);
     }
 }
 
